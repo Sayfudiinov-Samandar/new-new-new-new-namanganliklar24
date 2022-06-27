6b562afeb0f35c8466df0c36a4938572cfecfec0
sayfutdiinov <sayfutdinovsaman@gmail.com>
2022-06-27 16:40:32 +0500

    add new folder js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var elModalOpenButton = document.querySelector(".remember-link");
+var elModal = document.querySelector(".modal");
+
+elModalOpenButton.addEventListener("click", function(){
+    elModal.classList.add("modal-show");
+})
+
+
+var elModalCloseButton = document.querySelector(".modal-close-button");
+var elModalWindnow=document.querySelector(".modal-window")
+
+elModalCloseButton.addEventListener("click" , function (){
+  `  // elModalWindnow.classList.add("modal-window-close");
+    elModal.classList.remove("modal-show");
+})
+
+
+
